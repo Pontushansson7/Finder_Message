5024d0b04bbe3b968507d4a3fa935c14107174d6
eddo555 <43670839+eddo555@users.noreply.github.com>
2019-05-17 15:05:52 +0200

    Försök att inte lägga in i master innan allt funka
    Funkar inte ändå

---
+++ MessageService/MessageService/IService1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MessageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        List<Messageinfo> GetMessages();
+        [OperationContract]
+        void AddMessage(AddMessage newMessage);
+        [OperationContract]
+        string HelloMsg(string aString);
+    }
+
+
+    [DataContract]
+    public class Messageinfo
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public int SendingUserId { get; set; }
+        [DataMember]
+        public int RecievingUserId { get; set; }
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public System.DateTime Time { get; set; }
+        [DataMember]
+        public int ServiceId { get; set; }
+        [DataMember]
+        public string ServiceTitle { get; set; }
+        [DataMember]
+        public byte[] UserPicture { get; set; }
+    }
+
+    public class AddMessage
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public int SendingUserId { get; set; }
+        [DataMember]
+        public int RecievingUserId { get; set; }
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public System.DateTime Time { get; set; }
+        [DataMember]
+        public int ServiceId { get; set; }
+        [DataMember]
+        public string ServiceTitle { get; set; }
+        [DataMember]
+        public byte[] UserPicture { get; set; }
+    }
+}
---
+++ MessageService/MessageService/Service1.svc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MessageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private MessagesDBEntities db = new MessagesDBEntities();
+
+        public string HelloMsg(string aString)
+        {
+            string hellomsg = "Hello my friend";
+            return hellomsg;
+        }
+
+        void IService1.AddMessage(AddMessage newMessage)
+        {
+
+            using (db)
+            {
+                
+                db.Message.Add(new Message
+                {
+                    SendingUserId = newMessage.SendingUserId,
+                    RecievingUserId = newMessage.RecievingUserId,
+                    Message1 = newMessage.Message,
+                    ServiceId = newMessage.ServiceId,
+                    ServiceTitle = newMessage.ServiceTitle,
+                    Time = DateTime.Now //ovveride time i db, lägger in klientens tid i db
+                    
+
+                });
+                db.SaveChanges();
+            }
+        }
+
+        List<Messageinfo> IService1.GetMessages()
+        {
+            using (db)
+            {
+                var message = from m in db.Message select m;
+                List<Messageinfo> allmessages = new List<Messageinfo>();
+
+                foreach (var rad in message)
+                {
+                    allmessages.Add(new Messageinfo
+                    {
+                        Id = rad.Id,
+                        SendingUserId = rad.SendingUserId,
+                        RecievingUserId = rad.RecievingUserId,
+                        Message = rad.Message1,
+                        ServiceId = rad.ServiceId,
+                        ServiceTitle = rad.ServiceTitle,
+                        Time = rad.Time
+                    });
+
+                }
+                return allmessages;
+            }
+        }
+    }
+}
